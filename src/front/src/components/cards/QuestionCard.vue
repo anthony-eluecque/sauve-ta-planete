65f5544570060ea7581b6d5924753889dbeb755d
Pierre Crepin <pierrot.859@gmail.com>
2023-12-08 00:36:36 +0100

    front home page base

--- src/front/src/components/cards/QuestionCard.vue
+++ src/front/src/components/cards/QuestionCard.vue
@@ -1,16 +1,37 @@
 <template>
-    <v-card class="question-card">
-        <v-card-title class="text-center my-2">1KG de riz pollue t-il plus que 300g de boeuf ?</v-card-title>
-        <v-divider class="mx-12 mb-4"></v-divider>
+    <v-card class="question-card py-6" color="#353168">
+        <span class="d-block text-center text-h5 mx-6">1KG de riz pollue t-il plus que 300g de boeuf ?</span>
+        <v-divider class="mx-6 my-4" :thickness="5" style="opacity: 100% !important;"></v-divider>
+        <v-row no-gutters class="mx-6 mt-6">
+            <v-col cols="6">
+                <v-btn class="choice-btn" color="#C2C2C2">
+                    <v-icon icon="mdi-close" color="#1E3050" size="x-large"></v-icon>
+                </v-btn>
+            </v-col>
+            <v-col cols="6" align="right">
+                <v-btn class="choice-btn" color="#C2C2C2">
+                    <v-icon icon="mdi-check" color="#1E3050" size="x-large"></v-icon>
+                </v-btn>
+            </v-col>
+        </v-row>
     </v-card>
 </template>
 
 <style>
 .question-card {
-    max-width: 800px;
+    max-width: 400px;
     margin: auto;
     width: 100%;
     padding: 10px 5px;
     border-radius: 15px;
+}
+
+.choice-btn {
+    width: 60px;
+    height: 60px !important;
+}
+
+.mdi-close, .mdi-check {
+    font-size: xxx-large !important;
 }
 </style>
--- src/front/src/views/home/HomePage.vue
+++ src/front/src/views/home/HomePage.vue
@@ -1,17 +1,19 @@
 <template>
-    <v-img class="planet-img" src="/planet.png"></v-img>
-    <v-row no-gutters class="d-flex justify-center my-6">
-        <span class="text-h4 mr-2 font-weight-bold" style="line-height: 60px; color: white;">{{ score }}</span>
-        <img width="55" height="55" :src="coin"/>
-    </v-row>
-    <QuestionCard class="mt-12"/>
+    <v-card class="home-card">
+        <v-img class="planet-img" src="/planet.png"></v-img>
+        <v-row no-gutters class="d-flex justify-center my-6">
+            <span class="text-h4 mr-2 font-weight-bold" style="line-height: 60px; color: white;">{{ score }}</span>
+            <img width="55" height="55" :src="coin"/>
+        </v-row>
+        <QuestionCard class="mt-16"/>
+    </v-card>
 </template>
 
 <script lang="ts" setup>
 import QuestionCard from '@/components/cards/QuestionCard.vue';
 import { ref, computed, type Ref, type ComputedRef } from 'vue';
 
-const score: Ref<number> = ref(130)
+const score: Ref<number> = ref(131)
 
 const coin: ComputedRef<string> = computed(() => {
     if (score.value === 130){
@@ -27,8 +29,17 @@ body {
     background-color: $main-back-color;
 }
 
+.home-card {
+    background-color: black;
+    height: 96vh;
+    margin: 2vh auto;
+    border-radius: 15px;
+    max-width: 450px;
+    border: 4px white solid;
+}
+
 .planet-img {
-    max-height: 30vh;
+    max-height: 35vh;
     height: 100%;
     margin-top: 30px;
 }
