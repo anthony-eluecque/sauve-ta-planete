7110c76e865aa476f0d342feec9330995d363b2c
Alex Lovergne <lovergnealex@outlook.fr>
2023-12-08 00:01:24 +0100

    Ajout docker-compose + stylus

--- src/front/src/views/home/HomePage.vue
+++ src/front/src/views/home/HomePage.vue
@@ -7,3 +7,11 @@
 import QuestionCard from '@/components/cards/QuestionCard.vue';
 import ScoreBar from '@/components/menu/ScoreBar.vue';
 </script>
+
+<style lang="stylus">
+@import "../src/styles/variables.styl"
+body {
+    background-color: $main-back-color;
+}
+
+</style>
