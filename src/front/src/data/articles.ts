733888e0d45ca72f32472d7f57cc08e1741865da
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-12-08 03:29:05 +0100

    Feat: Added question data

---
+++ src/front/src/data/articles.ts
@@ -0,0 +1,92 @@
+export interface Article {
+    titre : string,
+    content : string
+    sources : string[]
+}
+
+export const articles : Article[] = [
+    {
+        titre : 'La pollution de l\'alimentation française',
+        content : `L'alimentation est une source importante de pollution en France. Elle représente environ 22 % de l'empreinte carbone de notre consommation totale, soit le troisième poste le plus émetteur de gaz à effet de serre. Les principales causes de cette pollution sont les suivantes :
+            La production agricole : l'agriculture est responsable de 61 % de l'empreinte carbone de l'alimentation française. Les principales sources de pollution sont les suivantes :
+            Les émissions de gaz à effet de serre, notamment dues à la déforestation, à l'utilisation d'engrais et de pesticides, et aux machines agricoles.
+            La pollution de l'eau, notamment due aux nitrates et aux pesticides.
+            La pollution des sols, notamment due aux pesticides et aux engrais.
+            Le transport des aliments : le transport des aliments représente environ 20 % de l'empreinte carbone de l'alimentation française. Il est responsable des émissions de gaz à effet de serre, de la pollution de l'air et de la consommation d'énergie.
+            La transformation des aliments : la transformation des aliments représente environ 10 % de l'empreinte carbone de l'alimentation française. Elle est responsable des émissions de gaz à effet de serre, de la pollution de l'eau et de la consommation d'énergie.
+            La consommation des aliments : la consommation des aliments représente environ 9 % de l'empreinte carbone de l'alimentation française. Elle est responsable de la pollution de l'eau, de la pollution de l'air et de la production de déchets.
+            La pollution de l'alimentation a des impacts négatifs sur l'environnement, la santé humaine et le bien-être animal. Elle contribue au changement climatique, à la pollution de l'eau et de l'air, et à la perte de biodiversité.
+            Les solutisons
+            Il existe de nombreuses solutions pour réduire la pollution de l'alimentation française. Ces solutions peuvent être mises en œuvre par les individus, les entreprises et les gouvernements.
+            Les individus peuvent contribuer à réduire la pollution de l'alimentation en adoptant les comportements suivants :
+            Consommer moins de viande : la viande est un aliment très polluant. En réduisant sa consommation de viande, on peut réduire son impact sur l'environnement.
+            Choisir des aliments locaux et de saison : les aliments locaux et de saison ont un impact environnemental moindre que les aliments importés ou hors saison.
+            Réduire le gaspillage alimentaire : le gaspillage alimentaire est une source importante de pollution. En réduisant le gaspillage alimentaire, on peut réduire son impact sur l'environnement.
+            Les entreprises peuvent contribuer à réduire la pollution de l'alimentation en adoptant les pratiques suivantes :
+            Développer l'agriculture durable : l'agriculture durable est une agriculture qui vise à réduire son impact sur l'environnement. Elle utilise des techniques telles que la rotation des cultures, l'agroforesterie et l'agriculture biologique.
+            Réduire les émissions de gaz à effet de serre : les entreprises peuvent réduire leurs émissions de gaz à effet de serre en utilisant des énergies renouvelables, en améliorant l'efficacité énergétique et en réduisant les déchets.
+            Réduire le gaspillage alimentaire : les entreprises peuvent réduire le gaspillage alimentaire en optimisant leurs processus de production et de distribution.
+            Les gouvernements peuvent contribuer à réduire la pollution de l'alimentation en adoptant les mesures suivantes :
+            Mettre en place des politiques de soutien à l'agriculture durable : les gouvernements peuvent soutenir l'agriculture durable en proposant des aides financières, des formations et des mesures de promotion.
+            Fixer des objectifs de réduction des émissions de gaz à effet de serre : les gouvernements peuvent fixer des objectifs de réduction des émissions de gaz à effet de serre pour le secteur de l'alimentation.
+            Mettre en place des mesures de lutte contre le gaspillage alimentaire : les gouvernements peuvent mettre en place des mesures telles que des campagnes de sensibilisation, des incitations financières et des mesures réglementaires pour lutter contre le gaspillage alimentaire.
+            La réduction de la pollution de l'alimentation est un défi important, mais il est possible de le relever. En adoptant des comportements responsables, les individus, les entreprises et les gouvernements peuvent contribuer à protéger l'environnement et la santé humaine.`,
+        sources : [
+            'https://www.notre-environnement.gouv.fr/actualites/breves/article/notre-alimentation-c-est-combien-de-gaz-a-effet-de-serre-ges',
+            'https://www.notre-environnement.gouv.fr/actualites/breves/article/alimentation-et-environnement-les-enjeux-de-la-consommation-de-viande-en-france',
+            'https://librairie.ademe.fr/consommer-autrement/779-empreinte-energetique-et-carbone-de-l-alimentation-en-france.html',
+            'https://agirpourlatransition.ademe.fr/particuliers/conso/conso-responsable/impact-alimentation-sur-environnement'
+        ]
+    },
+    {
+        titre: 'Climat en France',
+        content: `La France a un climat tempéré, avec des hivers doux et des étés chauds. Le pays est situé dans l'hémisphère nord, entre les latitudes 42 et 51 degrés nord. La côte atlantique et la côte méditerranéenne bénéficient d'un climat océanique, avec des précipitations régulières tout au long de l'année. L'intérieur du pays, en revanche, a un climat continental, avec des hivers froids et des étés chauds et secs.
+            Les températures moyennes en France varient en fonction de la région. À Paris, la température moyenne en janvier est de 2 degrés Celsius, et la température moyenne en juillet est de 25 degrés Celsius.
+            Les précipitations moyennes en France sont de 600 millimètres par an. Les régions les plus humides sont les côtes atlantique et méditerranéenne, qui reçoivent plus de 1 000 millimètres de précipitations par an. Les régions les plus sèches sont l'intérieur du pays, qui reçoivent moins de 500 millimètres de précipitations par an.
+            Le climat de la France est influencé par plusieurs facteurs, notamment la latitude, l'altitude, la proximité de l'océan et la circulation des vents.`,
+        sources : [
+            'https://agirpourlatransition.ademe.fr/',
+            'https://www.gouvernement.fr/les-priorites/france-nation-verte/la-planification-ecologique'
+        ]
+    },
+    {
+        titre : 'Histoire du changement climatique français',
+        content : `
+            Le changement climatique est un phénomène global qui affecte tous les pays du monde, y compris la France. Les températures moyennes en France ont augmenté de 1,2 degré Celsius depuis le début du XXe siècle. Cette augmentation est due à l'augmentation des émissions de gaz à effet de serre, telles que le dioxyde de carbone, le méthane et l'oxyde nitreux.
+            Les effets du changement climatique se font déjà sentir en France. Les étés sont plus chauds et plus secs, les hivers sont plus doux, et les précipitations sont plus irrégulières. Ces changements ont des conséquences sur l'environnement, la santé humaine et l'économie.
+            Le gouvernement français a pris des mesures pour lutter contre le changement climatique. En 2015, la France a ratifié l'Accord de Paris, un accord international visant à limiter le réchauffement climatique à 2 degrés Celsius. La France a également fixé un objectif de neutralité carbone d'ici 2050.
+            https://meteofrance.com/changement-climatique/observer/le-changement-climatique-en-france`,
+        sources : [
+            'https://meteofrance.com/changement-climatique/observer/le-changement-climatique-en-france',
+            'https://www.vie-publique.fr/eclairage/290911-chronologie-du-changement-climatique-dorigine-humaine#:~:text=URL%3A%20https%3A%2F%2Fwww.vie,explorer%E2%80%A0support.microsoft.com%E3%80%91'
+        ]
+    },
+    {
+        titre : 'Organisme en rapport avec la pollution française',
+        content : `
+            Il existe de nombreux organismes en rapport avec la pollution en France. Parmi les plus importants, on peut citer :
+            L'Agence de l'environnement et de la maîtrise de l'énergie (ADEME) : l'ADEME est un établissement public à caractère industriel et commercial (EPIC) placé sous la tutelle du ministère de la Transition écologique. Elle a pour mission de promouvoir l'environnement et la maîtrise de l'énergie.
+            L'Institut national de l'environnement industriel et des risques (INERIS) : l'INERIS est un établissement public à caractère scientifique et technique (EPST) placé sous la tutelle du ministère de la Transition écologique. Il a pour mission de fournir des expertises et des analyses sur les risques industriels et environnementaux.
+            L'Agence nationale de sécurité sanitaire de l'alimentation, de l'environnement et du travail (ANSES) : l'ANSES est un établissement public à caractère scientifique et technique (EPST) placé sous la tutelle des ministères de la Santé, de l'Agriculture et de la Transition écologique. Elle a pour mission de garantir la sécurité sanitaire de l'alimentation, de l'environnement et du travail.
+            Ces organismes ont des missions différentes, mais ils travaillent tous ensemble pour lutter contre la pollution en France.`,
+        sources : [
+            'https://www.ademe.fr/',
+            'https://www.ineris.fr/fr',
+            'https://www.anses.fr/fr'
+        ]
+    },
+    {
+        titre: 'Décès liés à la pollution',
+        content : `
+            Selon l'OMS, la pollution est responsable de 9 millions de décès prématurés par an dans le monde. En France, la pollution est responsable de 48 000 décès prématurés par an.
+            Les principales causes de décès liées à la pollution en France sont les suivantes :
+            Les maladies cardiovasculaires : la pollution de l'air est un facteur de risque important pour les maladies cardiovasculaires, telles que l'infarctus du myocarde et l'accident vasculaire cérébral.
+            Les maladies respiratoires : la pollution de l'air est également un facteur de risque important pour les maladies respiratoires, telles que l'asthme et la bronchopneumopathie chronique obstructive (BPCO).
+            Le cancer : la pollution de l'air peut augmenter le risque de certains cancers, tels que le cancer du poumon et le cancer du sein.
+            La pollution peut également avoir des effets négatifs sur le développement des enfants
+        `,
+        sources : [
+            'https://wp.towson.edu/iajournal/2017/02/27/consequences-of-air-pollution-in-france/'
+        ]
+    }
+]
